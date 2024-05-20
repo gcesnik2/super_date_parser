7d6c66284fbad56450693ec12d58bf29df1a3db0
Greg Cesnik <greg@twinsunsolutions.com>
2024-05-20 18:01:40 -0500

    Initial commit

---
+++ lib/super_date_parser2.rb
@@ -0,0 +1,6 @@
+require "super_date_parser2/version"
+
+module SuperDateParser2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/super_date_parser2/version.rb
@@ -0,0 +1,3 @@
+module SuperDateParser2
+  VERSION = "0.1.0"
+end
